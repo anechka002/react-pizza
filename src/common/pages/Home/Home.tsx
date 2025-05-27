ac56aa69ba70772236a842334a9d8ab95c94b3fa
Anna <blinova.anneta@gmail.com>
2025-05-27 09:27:09 +0200

    params

--- src/common/pages/Home/Home.tsx
+++ src/common/pages/Home/Home.tsx
@@ -1,29 +1,35 @@
-import { SearchContext } from "@/app/App";
-import { selectCategoryId, selectSort } from "@/app/redux/slices/filterSlice";
+import { selectCategoryId, selectSearch, selectSort } from "@/app/redux/slices/filterSlice";
 import { Categories, Pagination, PizzaBlock, PizzaSkeleton, Sort } from "@/common/components";
 import { useAppSelector } from "@/common/hooks";
 import type { PizzasType } from "@/common/types";
 import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
-  const {searchValue} = useContext(SearchContext)
+
   const [items, setItems] = useState<PizzasType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [currentPage, setCurrentPage] = useState<number>(1)
 
   const categoryId = useAppSelector(selectCategoryId)
   const sortType = useAppSelector(selectSort)
+  const searchValue = useAppSelector(selectSearch)
 
   useEffect(() => {
     setIsLoading(true);
-      axios
-        .get(`https://682df928746f8ca4a47b67c3.mockapi.io/items?page=${currentPage}&limit=4${categoryId > 0 ? `&category=${categoryId}` : ''}&sortBy=${sortType.sortProperty}&order=desc${searchValue ? `&search=${searchValue}` : ''}`)
-        .then((response) => {
-          setItems(response.data);
-          setIsLoading(false);
-        });
-      window.scrollTo(0, 0)
+
+    const sortBy = sortType.sortProperty.replace('-', '');
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
+    const category = categoryId > 0 ? `category=${categoryId}` : ''
+    const search = searchValue ? `&search=${searchValue}` : ''
+
+    axios.get(`https://682df928746f8ca4a47b67c3.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`)
+      .then((response) => {
+        setItems(response.data);
+        setIsLoading(false);
+      });
+      
+    window.scrollTo(0, 0)
   }, [categoryId, sortType, searchValue, currentPage]);
     
   return (
