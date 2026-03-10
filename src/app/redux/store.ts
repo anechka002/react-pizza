9717f1769bb3a985c7e40a523eb4ffb2b79192f3
Anna <blinova.anneta@gmail.com>
2026-03-10 10:31:17 +0100

    chore(store): remove browser debug binding

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -15,7 +15,3 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // автоматическое определение типа метода dispatch
 export type AppDispatch = typeof store.dispatch
-
-// для возможности обращения к store в консоли браузера
-// @ts-ignore
-window.store = store
